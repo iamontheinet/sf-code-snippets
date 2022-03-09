435f4e8830062cfdd73a787a60d008f12dfc0e04
Dash Desai <1723932+iamontheinet@users.noreply.github.com>
2022-03-09 12:28:17 -0800

    Add files via upload

---
+++ aws_secrets_manager_sf_connection.py
@@ -0,0 +1,43 @@
+### Pre-reqs: 
+# pip install boto3 awscli
+# Set AWS credentials via environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+###
+
+import boto3
+from botocore.exceptions import ClientError
+
+# Load Snowflake connection details from AWS Secrets Manager
+def get_aws_sf_connection_details(secret_name,region_name):
+    
+    # Create a Secrets Manager boto3 client
+    boto3_session = boto3.session.Session()
+    client = boto3_session.client(service_name='secretsmanager',region_name=region_name)
+    
+    get_secret_value_response = None
+
+    try:
+        # Get secret values(s) based on the passed in secret name
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)['SecretString']
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            raise e
+
+    return get_secret_value_response
+        
+# Create Snowflake Session object
+connection_parameters = json.loads(get_aws_sf_connection_details('AWS_SECRET_NAME','AWS_REGION'))
+session = Session.builder.configs(connection_parameters).create()
+print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
---
+++ azure_key_vault_sf_connection.py
@@ -0,0 +1,41 @@
+### Pre-reqs: 
+# pip install azure-keyvault-secrets azure-identity azure-cli
+# Log into your Azure account via Azure CLI (i.e. az login) 
+###
+
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+# Load Snowflake connection details from Azure Key Vault
+def get_azure_sf_connection_details(vault_url):
+    
+    # Create DefaultAzureCredential credential client 
+    credential = DefaultAzureCredential(
+        exclude_azurecli_credential=False,
+        exclude_environment_credential=True,
+        exclude_managed_identity_credential=True,
+        exclude_powershell_credential=True,
+        exclude_visual_studio_code_credential=True,
+        exclude_shared_token_cache_credential=True,
+        exclude_interactive_browser_credential=True
+    )
+    
+    # Get secret values(s) based on the passed in Key Vault URL
+    secret_client = SecretClient(vault_url=vault_url, credential=credential)
+    
+    connection_params = {
+      "account"       : secret_client.get_secret('account').value,
+      "user"          : secret_client.get_secret('user').value,
+      "password"      : secret_client.get_secret('password').value,
+      "role"          : secret_client.get_secret('role').value,
+      "warehouse"     : secret_client.get_secret('warehouse').value,
+      "database"      : secret_client.get_secret('database').value,
+      "schema"        : secret_client.get_secret('schema').value
+    }
+
+    return connection_params
+
+# Create Snowflake Session object
+connection_parameters = get_azure_sf_connection_details('YOUR_AZURE_VAULT_URL')
+session = Session.builder.configs(connection_parameters).create()
+print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
